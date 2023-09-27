75fd08b1b719eb2d97275b039a3a17297a11612c
nikita23092000 <nik.2000.57@mail.ru>
2023-09-27 17:58:23 +0300

    first commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
 export default function HomePage() {
-    return <h1>Hello, Next.js!</h1>;
+    return <><h1>Hello, Next.js!</h1>
+    <input type='date'/></>;
   }
---
+++ pages/todo.jsx
@@ -0,0 +1,5 @@
+import ToDoApp from "../components/todo";
+
+export default function ToDoPage() {
+    return <ToDoApp/>
+}
